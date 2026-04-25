e72397206d2edb69fe1bda65d554a4dd98aa264e
system-hack-07 <nandoskarsamarth@gmail.com>
2026-04-25 10:06:32 +0530

    Updated api/info.js file with new function to handle Instagram user information retrieval.

--- api/info.js
+++ api/info.js
@@ -1,15 +1,63 @@
-// Instagram API handler
+addEventListener("fetch", event => {
+  event.respondWith(handle(event.request));
+});
 
-const axios = require('axios');
+async function handle(req) {
+  const url = new URL(req.url);
+  const name = url.searchParams.get("username");
 
-const getInstagramUserData = async (username) => {
+  if (!name) {
+    return json({ error: "missing_username" }, 400);
+  }
+
+  try {
+    const r = await fetch(
+      `https://www.instagram.com/${name}/?__a=1&__d=dis`,
+      {
+        headers: {
+          "User-Agent": "Mozilla/5.0"
+        }
+      }
+    );
+
+    const text = await r.text();
+
+    let data;
     try {
-        const response = await axios.get(`https://api.instagram.com/v1/users/search?q=${username}&access_token=YOUR_ACCESS_TOKEN`);
-        return response.data;
-    } catch (error) {
-        console.error('Error fetching data from Instagram API:', error);
-        throw error;
+      data = JSON.parse(text);
+    } catch (e) {
+      return json({
+        error: "instagram_blocked",
+        message: "Response not JSON"
+      });
     }
-};
 
-module.exports = { getInstagramUserData };
+    const user = data?.graphql?.user;
+
+    if (!user) {
+      return json({ error: "not_found" }, 404);
+    }
+
+    return json({
+      username: user.username,
+      name: user.full_name,
+      bio: user.biography,
+      pic: user.profile_pic_url_hd,
+      followers: user.edge_followed_by.count,
+      following: user.edge_follow.count,
+      posts: user.edge_owner_to_timeline_media.count
+    });
+
+  } catch (err) {
+    return json({ error: "server_error", msg: err.message }, 500);
+  }
+}
+
+function json(obj, status = 200) {
+  return new Response(JSON.stringify(obj), {
+    status,
+    headers: {
+      "Content-Type": "application/json"
+    }
+  });
+}
